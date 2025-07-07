6d0d516567814bb072d7f711fd7e938f5809d209
roshan_singh <singhroshandu@gmail.com>
2025-07-07 23:08:26 +0530

    fix: franchise migration fix

--- config/config.js
+++ config/config.js
@@ -6,13 +6,15 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    logging:false
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    logging:false,
   }
 };
